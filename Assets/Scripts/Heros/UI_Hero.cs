5452b0ce0ddcc229487e7df36a21fb1592cce632
Kusmic <damien.harle@gmail.com>
2023-10-28 18:08:43 +0200

    added timer & les autres trucs là

--- Assets/Scripts/Heros/UI_Hero.cs
+++ Assets/Scripts/Heros/UI_Hero.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UI_Hero : MonoBehaviour
@@ -8,9 +9,24 @@ public class UI_Hero : MonoBehaviour
     public GameObject heartPrefab;
     List<UI_HeroHeart> hearts = new();
 
+    public GameObject ItemBar;
+    public GameObject itemsPrefab;
+    List<UI_HeroItem> items = new();
+    public string[] itemNamesChoices;
+    
+    public TMP_Text timeLeftBeforeNextMove;
+    public TMP_Text heroPersonality;
+    public TMP_Text heroLevel;
+    public TMP_Text heroName;
+
 
     [Header("Test Value to delete")] public float playerHealth = 6;
     public float currentHealth = 6;
+
+    public int itemSlotsCount = 3;
+    private float timeLeft = 0f;
+
+    #region Health
 
     public void DrawHearts()
     {
@@ -22,7 +38,7 @@ public class UI_Hero : MonoBehaviour
         {
             CreateEmptyHeart();
         }
-        
+
         for (int i = 0; i < hearts.Count; i++)
         {
             int heartState = (int)Mathf.Clamp(currentHealth - i * 2, 0, 2);
@@ -63,16 +79,75 @@ public class UI_Hero : MonoBehaviour
 
         hearts.Clear();
     }
+
+    #endregion
+
+    #region Items
+
+    private void DrawItems()
+    {
+        DestroyAllItems();
+        for (int i = 0; i < itemSlotsCount; i++)
+        {
+            CreateEmptyItem();
+        }
+        
+        for (int i = 0; i < items.Count; i++)
+        {
+            items[i].SetItem(itemNamesChoices[i]);
+        }
+    }
+
+    private void CreateEmptyItem()
+    {
+        GameObject item = Instantiate(itemsPrefab, ItemBar.transform);
+        UI_HeroItem itemScript = item.GetComponent<UI_HeroItem>();
+        items.Add(itemScript);
+    }
+
+    private void DestroyAllItems()
+    {
+        foreach (UI_HeroItem item in items)
+        {
+            Destroy(item.gameObject);
+        }
+
+        items.Clear();
+    }
+
+    #endregion
+
+    public void SetHeroData(HeroData heroData)
+    {
+        heroName.text = heroData.heroName;
+        heroLevel.text = heroData.heroLevel.ToString();
+        heroPersonality.text = heroData.heroPersonality;
+    }
+    
+    public void UpdateTimeLeftBeforeNextMove(float time)
+    {
+        timeLeftBeforeNextMove.text = time.ToString("0.0");
+    }
     
     private void Start()
     {
         DrawHearts();
+        DrawItems();
+        
+        
+        //test hero data
+        HeroData heroData = new HeroData();
+        heroData.heroName = "Hero's Name";
+        heroData.heroLevel = 1;
+        heroData.heroPersonality = "Hero's Personality";
+        SetHeroData(heroData);
     }
-    
-    
+
+
     //test update will be deleted later
     private void Update()
     {
+        //test health
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             currentHealth--;
@@ -83,5 +158,24 @@ public class UI_Hero : MonoBehaviour
             currentHealth++;
             DrawHearts();
         }
+        
+        //test time
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            timeLeft = 10f;
+        }
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            UpdateTimeLeftBeforeNextMove(timeLeft);
+        }
     }
+}
+
+//test class will be deleted later
+public class HeroData
+{
+    public string heroName;
+    public int heroLevel;
+    public string heroPersonality;
 }
