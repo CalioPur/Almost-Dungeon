fc435c899095adf65426983e56414d45428be127
esdragor <baudouinlapeze9@gmail.com>
2023-10-28 16:08:55 +0200

    Spawn random tiles

--- Assets/Scripts/Map/CreateMap.cs
+++ Assets/Scripts/Map/CreateMap.cs
@@ -40,7 +40,7 @@ public class CreateMap : MonoBehaviour
 
             }
         }
-        DeckManager.OnCardTryToPlaceEvent += CheckPos;
+        DeckManager.OnCardTryToPlaceEvent += CheckCardPos;
         
         
         
@@ -55,26 +55,37 @@ public class CreateMap : MonoBehaviour
                 cardInstance.addRotation();
                 print("iii");
             }
-        
-            //CheckPos(mapArray[Random.Range(0,width-2), Random.Range(0, height-2)], );
+
+            int x = 0;
+            int y = 0;
+            
+            do
+            {
+                y = Random.Range(1, height - 3);
+                x = Random.Range(1, width - 3);
+            } while (mapArray[x, y].PiecePlaced && !CheckPosWithPosition(x, y, cardInstance));
+            
+            SetTileAtPosition(cardInstance, x, y);
         }
+    }
+
+    private void CheckCardPos(TileData data, CardHand card)
+    {
+        bool canBePlaced = true;
+        canBePlaced = CheckPosWithData(data, card);
+        OnCardTryToPlaceEvent?.Invoke(data, card, canBePlaced);
     }
     
     public Vector3 InitEnterDungeon(CardInfo card)
     {
         int y = UnityEngine.Random.Range(0, height - 2);
         
-        mapArray[0, y].PiecePlaced = true;
-        mapArray[0, y].hasDoorRight = card.DoorOnRight;
-        mapArray[0, y].hasDoorLeft = card.DoorOnLeft;
-        mapArray[0, y].hasDoorUp = card.DoorOnTop;
-        mapArray[0, y].hasDoorDown = card.DoorOnBottom;
-        mapArray[0, y].GetComponent<TileData>().img.sprite = card.img;
+        SetTileAtPosition(card, 0, y);
         
         return new Vector3(1 - ((float)(width - 1) / 2), 0.1f, 1 + y- (float)(height - 1) / 2);
     }
 
-    private void CheckPos(TileData data, CardHand card)
+    private bool CheckPosWithData(TileData data, CardHand card)
     {
         bool canBePlaced = true;
         for(int i =0; i < width - 2; i++)
@@ -82,22 +93,36 @@ public class CreateMap : MonoBehaviour
             for (int j = 0; j < height - 2; j++)
             {
                 if (mapArray[i, j] != data) continue;
-                if (mapArray[i, j].PiecePlaced) canBePlaced = false;
-                else if (i > 0 && mapArray[i - 1, j].PiecePlaced && ((mapArray[i - 1, j].hasDoorRight && !card.Card.DoorOnLeft) || (!mapArray[i - 1, j].hasDoorRight && card.Card.DoorOnLeft))) canBePlaced = false;
-                else if (i < width - 3 && mapArray[i + 1, j].PiecePlaced &&  ((mapArray[i + 1, j].hasDoorLeft && !card.Card.DoorOnRight) || (!mapArray[i + 1, j].hasDoorLeft && card.Card.DoorOnRight))) canBePlaced = false;
-                else if (j > 0 && mapArray[i, j - 1].PiecePlaced &&  ((mapArray[i, j - 1].hasDoorUp && !card.Card.DoorOnBottom) || (!mapArray[i, j - 1].hasDoorUp && card.Card.DoorOnBottom))) canBePlaced = false;
-                else if (j < height - 3 && mapArray[i, j + 1].PiecePlaced &&  ((mapArray[i, j + 1].hasDoorDown && !card.Card.DoorOnTop) || (!mapArray[i, j + 1].hasDoorDown && card.Card.DoorOnTop))) canBePlaced = false;
-
-                OnCardTryToPlaceEvent?.Invoke(data, card, canBePlaced);
-                return;
+                return CheckPosWithPosition(i, j, card.Card);
             }
         }
-        OnCardTryToPlaceEvent?.Invoke(data, card, false);
+        return true;
+    }
+
+    private bool CheckPosWithPosition(int x, int y, CardInfo card)
+    {
+        if (mapArray[x, y].PiecePlaced) return false;
+        if (x > 0 && mapArray[x - 1, y].PiecePlaced && ((mapArray[x - 1, y].hasDoorRight && !card.DoorOnLeft) || (!mapArray[x - 1, y].hasDoorRight && card.DoorOnLeft))) return false;
+        if (x < width - 3 && mapArray[x + 1, y].PiecePlaced &&  ((mapArray[x + 1, y].hasDoorLeft && !card.DoorOnRight) || (!mapArray[x + 1, y].hasDoorLeft && card.DoorOnRight))) return false;
+        if (y > 0 && mapArray[x, y - 1].PiecePlaced &&  ((mapArray[x, y - 1].hasDoorUp && !card.DoorOnBottom) || (!mapArray[x, y - 1].hasDoorUp && card.DoorOnBottom))) return false;
+        if (y < height - 3 && mapArray[x, y + 1].PiecePlaced &&  ((mapArray[x, y + 1].hasDoorDown && !card.DoorOnTop) || (!mapArray[x, y + 1].hasDoorDown && card.DoorOnTop))) return false;
+        
+        return true;
     }
 
     public TileData GetTileData(int x, int y)
     {
         return mapArray[x, y];
+    }
+
+    void SetTileAtPosition(CardInfo card, int posX, int posY)
+    {
+        mapArray[posX, posY].PiecePlaced = true;
+        mapArray[posX, posY].hasDoorRight = card.DoorOnRight;
+        mapArray[posX, posY].hasDoorLeft = card.DoorOnLeft;
+        mapArray[posX, posY].hasDoorUp = card.DoorOnTop;
+        mapArray[posX, posY].hasDoorDown = card.DoorOnBottom;
+        mapArray[posX, posY].GetComponent<TileData>().img.sprite = card.img;
     }
     
 }
