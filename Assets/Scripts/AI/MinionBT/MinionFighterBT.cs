b0dd8d204e70b075b26f2801b99aed6abf56c64f
CalioPur <a.marcelli@rubika-edu.com>
2023-10-31 18:54:05 +0100

    preparation du terrain pour la terreur

---
+++ Assets/Scripts/AI/MinionBT/MinionFighterBT.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourTree;
+using Tree = BehaviourTree.Tree;
+
+public class MinionFighterBT : Tree
+{
+    protected override Node InitTree()
+    {
+        throw new System.NotImplementedException();
+    }
+}
