5b06dab9a139e9bbce96501c591e4594c6d01296
esdragor <64788862+esdragor@users.noreply.github.com>
2023-11-02 15:29:35 +0100

    hero spawn after first tick + center tile begin

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,40 +6,42 @@ using Random = UnityEngine.Random;
 public class GameManager : MonoBehaviour
 {
     public static Action OnBeginToMoveEvent;
-    
-    [Header("Managers")]
-    [SerializeField] private MapManager mapManager;
+
+    [Header("Managers")] [SerializeField] private MapManager mapManager;
     [SerializeField] private TickManager tick;
-    
-    [Header("Data")]
-    [SerializeField] private List<HeroesInfo> heroesInfos;
+
+    [Header("Data")] [SerializeField] private List<HeroesInfo> heroesInfos;
     [SerializeField] private CardInfo enterDungeonInfo;
     [SerializeField] private Hero heroRenderer;
+    [SerializeField] private Vector2Int startPosHero;
 
     private bool WaitForBeginToMove = true;
-
+    private Vector3 pos;
 
     void Start()
     {
-        MapManager.OnCardTryToPlaceEvent += CheckIsFirstMove; 
-        
+        MapManager.OnCardTryToPlaceEvent += CheckIsFirstMove;
+
         mapManager.InitMap();
-        Vector3 pos;
-        int indexHeroX;
-        int indexHeroY;
-        mapManager.InitEnterDungeon(enterDungeonInfo.CreateInstance(), out pos, out indexHeroX, _y: out indexHeroY);
+        mapManager.InitEnterDungeon(enterDungeonInfo.CreateInstance(), out pos, startPosHero);
         pos += new Vector3(1, 0.1f, 1); //pour que le hero soit au dessus du sol
         mapManager.AddRandomCard();
+    }
+
+    private void SpawnHero()
+    {
         int randomHero = Random.Range(0, heroesInfos.Count);
         HeroInstance current = heroesInfos[randomHero].CreateInstance();
-        
-        Instantiate(heroRenderer, pos, heroRenderer.transform.rotation).Init(current, indexHeroX, indexHeroY, mapManager);
+
+        Instantiate(heroRenderer, pos, heroRenderer.transform.rotation)
+            .Init(current, startPosHero.x, startPosHero.y, mapManager);
     }
 
     private void CheckIsFirstMove(TileData _, CardHand __, bool ___)
     {
         WaitForBeginToMove = false;
         MapManager.OnCardTryToPlaceEvent -= CheckIsFirstMove;
+        SpawnHero();
         OnBeginToMoveEvent?.Invoke();
     }
 }
