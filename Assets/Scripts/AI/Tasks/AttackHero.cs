1fe43dc3a0c8d31cbc0b4c124ab713590b121154
esdragor <baudouinlapeze9@gmail.com>
2023-11-18 17:11:41 +0100

    anim attack in direction and not hero position

--- Assets/Scripts/AI/Tasks/AttackHero.cs
+++ Assets/Scripts/AI/Tasks/AttackHero.cs
@@ -20,7 +20,9 @@ public class AttackHero : Node
             Vector3 heroPosVec3 = Vector3.zero;
             blackboard.minionData.mapManager.GetWorldPosFromTilePos(
                 new Vector2Int(blackboard.heroPosition.x + 1, blackboard.heroPosition.y + 1), out heroPosVec3);
-            blackboard.minionData.addAnim(new AnimToQueue(blackboard.minionData.transform, heroPosVec3, 0.3f,
+            Vector3 dir = heroPosVec3 - blackboard.minionData.transform.position;
+            
+            blackboard.minionData.addAnim(new AnimToQueue(blackboard.minionData.transform, blackboard.minionData.transform.position + dir.normalized, 0.3f,
                 Ease.InBack, 2));
             blackboard.minionData.Attack(blackboard.minionData.minionInstance.So.damage);
             return NodeState.Success;
