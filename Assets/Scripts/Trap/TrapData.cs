1f3ad2264006bf69d6c7f9f3c24ee276afdceaf5
esdragor <64788862+esdragor@users.noreply.github.com>
2023-11-19 15:51:20 +0100

    decal death minion

--- Assets/Scripts/Trap/TrapData.cs
+++ Assets/Scripts/Trap/TrapData.cs
@@ -25,7 +25,7 @@ public abstract class TrapData : MonoBehaviour
         mapManager.GetWorldPosFromTilePos(new Vector2Int(indexX, indexY) , out Vector3 worldPos);
         mapManager.RemoveEnemyOnTile(
             new Vector2Int(indexX, indexY), this, worldPos);
-        Destroy(gameObject);
+        Destroy(gameObject, 0.5f);
     }
 
     public void Attack(int damage)
