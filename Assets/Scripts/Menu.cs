f75a6efffff538bab645e41630d694de6476f4e9
esdragor <64788862+esdragor@users.noreply.github.com>
2023-11-29 11:22:39 +0100

    Menu V1

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,11 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+    
     void Start()
+    {
+        playButton.onClick.AddListener(Play);
+        quitButton.onClick.AddListener(Quit);
+    }
+
+    private void Quit()
+    {
+        Application.Quit();
+    }
+
+    private void Play()
     {
         SceneManager.LoadScene(1);
     }
