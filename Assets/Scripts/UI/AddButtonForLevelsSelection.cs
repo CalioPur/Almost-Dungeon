a1b857143febb6197ac5163689e507d71f60de40
Damien <damien.harle@gmail.com>
2023-12-07 12:48:33 +0100

    Main Screen

--- Assets/Scripts/UI/AddButtonForLevelsSelection.cs
+++ Assets/Scripts/UI/AddButtonForLevelsSelection.cs
@@ -2,24 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class AddButtonForLevelsSelection : MonoBehaviour
 {
     public GameObject buttonPrefab;
     public DungeonManager dungeonManager;
+    [SerializeField] private Image image;
+    [SerializeField] private VideoPlayer videoPlayer;
+    [SerializeField] private GameObject CanvasBlack;
+    [SerializeField] float timeToWait = 2f;
     
+    private List<GameObject> buttonList = new();
     void Start()
     {
         int cpt = 0;
         foreach (var biome in dungeonManager.dungeons)
         {
             GameObject theButton = Instantiate(buttonPrefab, transform);
+            buttonList.Add(theButton);
             theButton.GetComponentInChildren<TMP_Text>().text = biome.name;
             int biomeIndex = cpt;
-            theButton.GetComponent<Button>().onClick.AddListener(() => dungeonManager.SetSelectedBiome(biomeIndex));
+            theButton.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                buttonList.ForEach(button => button.SetActive(false));
+                image.color = Color.clear;
+                videoPlayer.playbackSpeed = 1;
+                videoPlayer.Play();
+                CanvasBlack.SetActive(true);
+                StartCoroutine(AlphaLerp(biomeIndex));
+            });
             cpt++;
         }
+    }
+    private IEnumerator AlphaLerp(int biomeIndex)
+    {
+        var alpha = CanvasBlack.GetComponent<Image>().color.a;
+        while (alpha < 1)
+        {
+            alpha += Time.deltaTime;
+            CanvasBlack.GetComponent<Image>().color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+        dungeonManager.SetSelectedBiome(biomeIndex);
+        yield break;
     }
 }
