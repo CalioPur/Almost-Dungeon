5556ad1106197ec43e1b09af911710ee879c3a62
Mcintyre Zachary <z.mcintyre@rubika-edu.com>
2023-12-21 12:54:51 +0100

    Build Milestone 2
    LD Donjon 2 et tweaks

--- Assets/Scripts/UI/AddButtonForLevelsSelection.cs
+++ Assets/Scripts/UI/AddButtonForLevelsSelection.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
-using UnityEditor.PackageManager.UI;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
