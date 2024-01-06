c86aee271a6e5b9de2ea5ee50a6844ed2e05c2c7
CalioPur <a.marcelli@rubika-edu.com>
2024-01-06 05:05:09 +0100

    ajout des choix dans les dialogues

---
+++ Assets/Dialogue/RKnightA2.ink
@@ -0,0 +1,20 @@
+Haaa... Haa... #chara:knight
+Tiens... Toujours vivant Chevalier ? #chara:dragon
+Haha... Bien sûr, ce n'était qu'un échauffement pour moi, je suis toujours prêt à te pourfendre ! #chara:knight
+
+* L'effrayer : -> Effrayer
+* Le rassurer : -> Rassurer
+
+=== Effrayer ===
+Bien, nous allons pouvoir augmenter le rythme alors. J'espère que vous êtes prêt à affronter mon armée. #chara:dragon
+Quoi... ? Euh... je veux dire bien sûr hahaha... une armée.. #chara:knight
+Tant mieux, commençons sans tarder, en garde Héros... #chara:dragon
+En garde Dragon ! #chara:knight
+    -> END
+
+=== Rassurer ===
+Mais oui vous allez y arriver ne vous en faites pas. Il suffit de persévérer toujours plus...  #chara:dragon
+Ah vraiment ? Euh... je veux dire bien sûr que je vais y arriver hahaha... #chara:knight
+Tant mieux, commençons sans tarder, en garde Héros... #chara:dragon
+En garde Dragon ! #chara:knight
+-> END
--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -21,9 +21,12 @@ public class DialogueManager : MonoBehaviour
     private Story story;
     private GameObject dialogueBox;
     private GameObject arrowKnight;
-    private GameObject arrowDragon; 
+    private GameObject arrowDragon;
+    private GameObject choice1;
+    private GameObject choice2;
     private TMP_Text dialogueText;
     private Button nextButton;
+    
     
     private void Awake()
     {
@@ -42,6 +45,8 @@ public class DialogueManager : MonoBehaviour
         dialogueBox = GameObject.Find("DialoguePanel");
         arrowKnight = GameObject.Find("ArrowKnight");
         arrowDragon = GameObject.Find("ArrowDragon");
+        choice1 = GameObject.Find("Choice1");
+        choice2 = GameObject.Find("Choice2");
         dialogueText = dialogueBox.GetComponentInChildren<TMP_Text>();
         nextButton = dialogueBox.GetComponentInChildren<Button>();
         nextButton.onClick.AddListener(NextDialogue);
@@ -68,7 +73,8 @@ public class DialogueManager : MonoBehaviour
         
         
         dialogueBox.SetActive(true);
-        
+        choice1.SetActive(false);
+        choice2.SetActive(false);
         RefreshView();
         Time.timeScale = 0;
     }
@@ -80,19 +86,56 @@ public class DialogueManager : MonoBehaviour
 
     void RefreshView()
     {
+        var choices = story.currentChoices.Count;
+        
+        
+        
         if (story.canContinue)
         {
             dialogueText.text = story.Continue();
             if (story.currentTags[0].Contains("knight")) ShowArrowKnight();
             else ShowArrowDragon();
+            
+            
         }
         else
         {
+            if(choices > 0)
+            {
+                DisplayChoices();
+                return;
+            }
             dialogueBox.SetActive(false);
             nextButton.onClick.RemoveAllListeners();
             story = null;
             Time.timeScale = 1;
         }
+    }
+
+    private void DisplayChoices()
+    {
+        DisplayChoices(story.currentChoices);
+    }
+
+    private void DisplayChoices(List<Choice> storyCurrentChoices)
+    {
+        choice1.SetActive(true);
+        choice2.SetActive(true);
+        nextButton.gameObject.SetActive(false);
+        choice1.GetComponentInChildren<TMP_Text>().text = storyCurrentChoices[0].text;
+        choice2.GetComponentInChildren<TMP_Text>().text = storyCurrentChoices[1].text;
+        choice1.GetComponentInChildren<Button>().onClick.AddListener(delegate { OnClickChoiceButton(storyCurrentChoices[0]); });
+        choice2.GetComponentInChildren<Button>().onClick.AddListener(delegate { OnClickChoiceButton(storyCurrentChoices[1]); });
+    }
+    
+    void OnClickChoiceButton (Choice choice) {
+        story.ChooseChoiceIndex (choice.index);
+        choice1.GetComponentInChildren<Button>().onClick.RemoveAllListeners();
+        choice2.GetComponentInChildren<Button>().onClick.RemoveAllListeners();
+        choice1.SetActive(false);
+        choice2.SetActive(false);
+        nextButton.gameObject.SetActive(true);
+        RefreshView();
     }
     
     void ShowArrowKnight()
