f8207c67a66394cf3e63535c760ea5ae9ec29ae7
CalioPur <a.marcelli@rubika-edu.com>
2024-01-15 14:12:27 +0100

    ajout des locks

--- Assets/LevelCardScript.cs
+++ Assets/LevelCardScript.cs
@@ -4,14 +4,34 @@ using System.Collections.Generic;
 using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelCardScript : MonoBehaviour
 {
     public Transform SpriteAttached;
+    public Sprite lockedSprite;
+    public bool isLocked = false;
     public int biomeIndex;
+
+    void Start()
+    {
+        if (DungeonManager._instance.dungeons[biomeIndex].isLocked)
+        {
+            SpriteAttached.GetComponent<SpriteRenderer>().sprite = lockedSprite;
+            isLocked = true;
+        }
+    }
+    
     private void OnMouseEnter()
     {
-        SpriteAttached.DOLocalMove(new Vector3(0, 0f, -1), 0.1f);
+        if (!isLocked)
+        {
+            SpriteAttached.DOLocalMove(new Vector3(0, 0f, -1), 0.1f);
+        }
+        else
+        {
+            SpriteAttached.DOLocalRotate(new Vector3(0, 0, 5), 0.05f).SetLoops(4, LoopType.Yoyo);
+        }
     }
 
     private void OnMouseOver()
@@ -26,6 +46,7 @@ public class LevelCardScript : MonoBehaviour
     private void OnMouseExit()
     {
         SpriteAttached.DOLocalMove(new Vector3(0, 0f, 0f), 0.1f);
+        SpriteAttached.DOLocalRotate(new Vector3(0, 0, 0), 0.1f);
     }
 
 
