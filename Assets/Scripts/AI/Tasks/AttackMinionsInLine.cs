5d76df6f23344d2c98b572b9feac3dc42b876046
esdragor <baudouinlapeze9@gmail.com>
2024-01-18 14:11:22 +0100

    mage new BT

---
+++ Assets/Scripts/AI/Tasks/AttackMinionsInLine.cs
@@ -0,0 +1,122 @@
+using System.Collections;
+using System.Collections.Generic;
+using BehaviourTree;
+using DG.Tweening;
+using UnityEngine;
+
+public class AttackMinionsInLine : Node
+{
+    HeroBlackboard blackboard;
+
+    public AttackMinionsInLine(HeroBlackboard _blackboard)
+    {
+        blackboard = _blackboard;
+    }
+
+    public override NodeState Evaluate(Node root)
+    {
+        Vector2Int indexes = blackboard.hero.GetIndexHeroPos();
+        Vector2Int MapSize = blackboard.hero.mapManager.GetSizeDungeon();
+
+
+        blackboard.Targets.Clear();
+        switch (blackboard.directionToMove)
+        {
+            case DirectionToMove.Up:
+                for (int y = indexes.y; y < MapSize.y; y++)
+                {
+                    TileData tileData = blackboard.hero.mapManager.GetTileDataAtPosition(indexes.x, y);
+                    if (!tileData.PiecePlaced) break;
+                    if (!tileData) break;
+
+                    if (blackboard.hero.mapManager.GetNbMonstersOnPos(new Vector2Int(indexes.x, y)) > 0)
+                    {
+                        foreach (var enemy in tileData.enemies)
+                        {
+                            blackboard.Targets.Add(enemy);
+                        }
+                    }
+
+                    if (!tileData.hasDoorUp) break;
+                }
+
+                break;
+            case DirectionToMove.Right:
+                for (int x = indexes.x; x < MapSize.x; x++)
+                {
+                    TileData tileData = blackboard.hero.mapManager.GetTileDataAtPosition(x, indexes.y);
+                    if (!tileData.PiecePlaced) break;
+                    if (blackboard.hero.mapManager.GetNbMonstersOnPos(new Vector2Int(x, indexes.y)) > 0)
+                    {
+                        foreach (var enemy in tileData.enemies)
+                        {
+                            blackboard.Targets.Add(enemy);
+                        }
+                    }
+
+                    if (!tileData.hasDoorRight) break;
+                }
+
+                break;
+            case DirectionToMove.Down:
+                for (int y = indexes.y; y >= 0; y--)
+                {
+                    TileData tileData = blackboard.hero.mapManager.GetTileDataAtPosition(indexes.x, y);
+                    if (!tileData.PiecePlaced) break;
+                    if (!tileData) break;
+
+                    if (blackboard.hero.mapManager.GetNbMonstersOnPos(new Vector2Int(indexes.x, y)) > 0)
+                    {
+                        foreach (var enemy in tileData.enemies)
+                        {
+                            blackboard.Targets.Add(enemy);
+                        }
+                    }
+
+                    if (!tileData.hasDoorDown) break;
+                }
+
+                break;
+            case DirectionToMove.Left:
+                for (int x = indexes.x; x >= 0; x--)
+                {
+                    TileData tileData = blackboard.hero.mapManager.GetTileDataAtPosition(x, indexes.y);
+                    if (!tileData.PiecePlaced) break;
+                    if (blackboard.hero.mapManager.GetNbMonstersOnPos(new Vector2Int(x, indexes.y)) > 0)
+                    {
+                        foreach (var enemy in tileData.enemies)
+                        {
+                            blackboard.Targets.Add(enemy);
+                        }
+                    }
+
+                    if (!tileData.hasDoorLeft) break;
+                }
+
+                break;
+            case DirectionToMove.None:
+                return NodeState.Failure;
+            case DirectionToMove.Error:
+                return NodeState.Failure;
+        }
+
+        if (blackboard.Targets.Count == 0)
+        {
+            return NodeState.Failure;
+        }
+
+        float delay = 0.5f;
+        blackboard.hero.PlayAttackClip();
+        blackboard.hero.PlayAttackFX(blackboard.Targets[^1].transform, 0.5f, blackboard.directionToMove);
+        blackboard.hero.AddAnim(new AnimToQueue(blackboard.hero.transform, blackboard.Targets[^1].transform,
+            Vector3.zero, true,
+            0.5f, Ease.InBack, 2));
+        foreach (var enemy in blackboard.Targets)
+        {
+            if (enemy)
+                enemy.TakeDamage(blackboard.hero.info.So.AttackPoint, blackboard.hero.attackType);
+        }
+
+        return NodeState.Success;
+    }
+}
