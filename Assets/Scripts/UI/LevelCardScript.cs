1a98ea92322e0511290d9dba723fba465bbfeb7d
esdragor <baudouinlapeze9@gmail.com>
2024-01-26 17:48:37 +0100

    Terminal canvas refacto part 2

--- Assets/Scripts/UI/LevelCardScript.cs
+++ Assets/Scripts/UI/LevelCardScript.cs
@@ -1,9 +1,6 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using DG.Tweening;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
