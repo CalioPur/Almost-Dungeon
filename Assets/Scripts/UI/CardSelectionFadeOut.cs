d75942a07cf0362eaabc3e662637d25f454075d0
CalioPur <a.marcelli@rubika-edu.com>
2024-03-17 14:29:38 +0100

    le fade

--- Assets/Scripts/UI/CardSelectionFadeOut.cs
+++ Assets/Scripts/UI/CardSelectionFadeOut.cs
@@ -9,6 +9,13 @@ public class CardSelectionFadeOut : MonoBehaviour
     [SerializeField] private Image img;
     void Start()
     {
+        StartCoroutine(FadeOut());
+        
+    }
+    
+    private IEnumerator FadeOut()
+    {
+        yield return new WaitForSeconds(1f);
         img.DOFade(0, 2f).SetUpdate(true);
     }
     
