205a426936fc84739e4614731a7da800b1bf6ab2
CalioPur <a.marcelli@rubika-edu.com>
2024-03-19 15:27:01 +0100

    anim unlock du donjon

--- Assets/Scripts/UI/LevelCardScript.cs
+++ Assets/Scripts/UI/LevelCardScript.cs
@@ -11,6 +11,7 @@ public class LevelCardScript : MonoBehaviour
 {
     public SpriteRenderer SpriteAttached;
     public Sprite lockedSprite;
+    public Sprite normalSprite;
     public Sprite FinishedSprite;
     public Image BlackImage;
     public bool isLocked = false;
@@ -20,6 +21,9 @@ public class LevelCardScript : MonoBehaviour
     [SerializeField] private TMP_Text tryText;
     [SerializeField] private TMP_Text victoryText;
     [SerializeField] private TMP_Text biomeName;
+    
+    private bool canPlay = true;
+    private bool shouldPlay = false;
 
     void Start()
     {
@@ -31,6 +35,10 @@ public class LevelCardScript : MonoBehaviour
                 child.gameObject.SetActive(false);
             }
             isLocked = true;
+            if (PlayerPrefs.GetInt("LevelJustUnlock" + biomeIndex, 0) == 1)
+            {
+                shouldPlay = true;
+            }
         }
         else if (PlayerPrefs.GetInt("LevelBeaten" + biomeIndex,0) == 1)
         {
@@ -50,6 +58,10 @@ public class LevelCardScript : MonoBehaviour
         {
             SpriteAttached.transform.DOLocalMove(new Vector3(0, 0f, -1), 0.1f);
         }
+        else if (PlayerPrefs.GetInt("LevelJustUnlock" + biomeIndex, 0) == 1)
+        {
+            //on fait rien
+        }
         else
         {
             SpriteAttached.transform.DOLocalRotate(new Vector3(0, 0, 5), 0.05f).SetLoops(4, LoopType.Yoyo);
@@ -62,7 +74,15 @@ public class LevelCardScript : MonoBehaviour
         {
             soundManagerIngame.PlayDialogueSFX("UiClick");
             //DungeonManager._instance.SetSelectedBiome(biomeIndex);
+            
             StartCoroutine(StartLevel());
+        }
+        else if (Input.GetMouseButtonDown(0) && isLocked && PlayerPrefs.GetInt("LevelJustUnlock" + biomeIndex, 0) == 1)
+        {
+            
+            shouldPlay = false;
+            canPlay = true;
+            if(canPlay) StartCoroutine(UnlockAnim());
         }
     }
 
@@ -70,6 +90,14 @@ public class LevelCardScript : MonoBehaviour
     {
         SpriteAttached.transform.DOLocalMove(new Vector3(0, 0f, 0f), 0.1f);
         SpriteAttached.transform.DOLocalRotate(new Vector3(0, 0, 0), 0.1f);
+    }
+
+    private void Update()
+    {
+        if(shouldPlay && canPlay)
+        {
+            StartCoroutine(UnlockAnimPump());
+        }
     }
 
     IEnumerator StartLevel()
@@ -81,4 +109,43 @@ public class LevelCardScript : MonoBehaviour
         PlayerPrefs.SetInt("LevelTry" + biomeIndex, PlayerPrefs.GetInt("LevelTry" + biomeIndex, 0) + 1);
     }
     
+    IEnumerator UnlockAnimPump()
+    {
+        canPlay = false;
+        SpriteAttached.transform.DOScale(new Vector3(1.1f, 1.1f, 1.1f), 0.5f).SetLoops(2, LoopType.Yoyo);
+        yield return new WaitForSeconds(1.1f);
+        canPlay = true;
+    }
+    IEnumerator UnlockAnim()
+    {
+        canPlay = false;
+        Transform tf = SpriteAttached.transform;
+        
+        //go up, do a rotation, change sprite mid rot, go down
+        //reset scale
+        tf.DOScale(new Vector3(1, 1, 1), 0.1f);
+        
+        tf.DOLocalMoveZ(-1f, 0.2f);
+        yield return new WaitForSeconds(0.2f);
+        tf.DOLocalRotate(new Vector3(0, 180, 0), 0.1f);
+        yield return new WaitForSeconds(0.1f);
+        
+        foreach (Transform child in tf)
+        {
+            child.gameObject.SetActive(true);
+        }
+        tryText.text = PlayerPrefs.GetInt("LevelTry" + biomeIndex, 0).ToString();
+        victoryText.text = PlayerPrefs.GetInt("LevelVictory" + biomeIndex, 0).ToString();
+        biomeName.text = DungeonManager._instance.dungeons[biomeIndex].name;
+        SpriteAttached.sprite = normalSprite;
+        
+        
+        tf.DOLocalRotate(new Vector3(0, 360, 0), 0.1f);
+        yield return new WaitForSeconds(0.3f);
+        tf.DOLocalMoveZ(0f, 0.2f);
+        yield return new WaitForSeconds(0.2f);
+        PlayerPrefs.SetInt("LevelUnlock" + biomeIndex, 1);
+        canPlay = true;
+        isLocked = false;
+    }
 }
