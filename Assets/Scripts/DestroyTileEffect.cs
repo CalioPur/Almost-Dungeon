fc63d8dca4dd1f9a95cb334721b3b6f8de5084d0
baudouinlapeze <baudouinlapeze9@gmail.com>
2024-03-21 11:11:16 +0100

    physics in destruction tile on rage

--- Assets/Scripts/DestroyTileEffect.cs
+++ Assets/Scripts/DestroyTileEffect.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class DestroyTileEffect : MonoBehaviour
@@ -7,12 +8,23 @@ public class DestroyTileEffect : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 1.5f);
+        float value = 30000.0f;
+        float delay = 0.5f;
+        Destroy(gameObject, delay);
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        
+        rb.AddForce(new Vector3(Random.Range(-value, value), 
+            value * 100.0f, 
+            Random.Range(-value, value)), ForceMode.Impulse);
+        // transform.DOMove(transform.position + 
+        //                  new Vector3(Random.Range(-value, value), 
+        //                      Random.Range(1.0f, value), 
+        //                      Random.Range(-value, value)), delay * 0.9f).SetEase(Ease.InOutSine);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0, 0.1f, 0);
+        //transform.position += new Vector3(0, 0.1f, 0);
     }
 }
